cc214b795cb6b16d0c68985c255c2aea4d95cbcf
Leandro Cruz <leandro.saad@gmail.com>
2019-05-07 14:30:29 -0300

    Added InternalController.echo()

--- play/src/main/scala/xingu/commons/play/controllers/InternalController.scala
+++ play/src/main/scala/xingu/commons/play/controllers/InternalController.scala
@@ -12,4 +12,9 @@ class InternalController @Inject() (services: Services) extends InjectedControll
   def ping() = Action { count +=1 ; Ok(s"pong: $count") }
   def conf() = Action { Ok(services.conf().get[Configuration]("app").underlying.root().render(ConfigRenderOptions.concise())).as(MimeTypes.JSON) }
   def stat() = Action { Ok }
+  def echo() = Action { req =>
+    val qs = req.queryString.map({case (key, values) => s"'$key' = '${values.mkString(", ")}'"}).mkString(", ")
+    val result = s"uri: '${req.uri}'\npath: '${req.path}'\nquery: '$qs'"
+    Ok(result).withHeaders(req.headers.headers: _*)
+  }
 }
