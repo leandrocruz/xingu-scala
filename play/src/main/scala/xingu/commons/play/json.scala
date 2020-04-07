960689bda75593526ec7822812f4cf3eda40695c
Leandro Cruz <leandro.saad@gmail.com>
2020-04-07 15:47:56 -0300

    Working with json date formatting

--- play/src/main/scala/xingu/commons/play/json.scala
+++ play/src/main/scala/xingu/commons/play/json.scala
@@ -3,31 +3,22 @@ package xingu.commons.play
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-import play.api.libs.json.Reads.dateReads
-import play.api.libs.json.Writes.dateWrites
 import play.api.libs.json._
 
 object json {
 
   val dateTimePattern = "yyyyMMdd'T'HHmmss"
 
-  def localDateReads(pattern: String) = {
+  def localDateTimeReads(pattern: String): Reads[LocalDateTime] = {
     val formatter = DateTimeFormatter.ofPattern(pattern)
-    new Reads[LocalDateTime] {
-      override def reads(json: JsValue) = json match {
-        case JsString(s) => JsSuccess(LocalDateTime.parse(s, formatter))
-        case _           => JsError(s"Can't read LocalDateTime from '$json'")
-      }
+    (json: JsValue) => json match {
+      case JsString(s) => JsSuccess(LocalDateTime.parse(s, formatter))
+      case json => JsError(s"Can't read LocalDateTime from '$json'")
     }
   }
 
-  def localDateWrites(pattern: String) = {
+  def localDateTimeWrites(pattern: String): Writes[LocalDateTime] = {
     val formatter = DateTimeFormatter.ofPattern(pattern)
     (it: LocalDateTime) => JsString(formatter.format(it))
   }
-
-  implicit val DefaultDateReader = dateReads(dateTimePattern)
-  implicit val DefaultDateWriter = dateWrites(dateTimePattern)
-  implicit val DefaultLocalDateTimeWrites = localDateWrites(dateTimePattern)
-  implicit val DefaultLocalDateTimeReads  = localDateReads(dateTimePattern)
 }
